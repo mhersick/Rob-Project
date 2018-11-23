91760fad4e3a5629525eecf8b21356d109819fa8
e3 image group <info@e3imagegroup.com>
2018-11-23 12:50:20 -0800

    Update SQLQuery1.sql

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,2 +1,4 @@
 ﻿select * from AspNetUserRoles
  Test of changed file. This was done on GitHub.com
+ 
+ I just changed this file on the local machine, and now I am changing it on GitHub.com.  This should create a conflict when I do a pull from my local machine. 
