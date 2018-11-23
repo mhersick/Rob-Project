1d74ab4148d50ff0b1e17a7c14ede15590acd30c
rawrdata <rory@rawrdata.com>
2018-11-23 12:53:32 -0800

    test file change conflict

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,2 +1,5 @@
 ﻿select * from AspNetUserRoles
  Test of changed file. This was done on GitHub.com
+
+I am now making a change to the local file.
+After that I will change the same file on GitHub.com and that should create a conflict when I do my next pull
